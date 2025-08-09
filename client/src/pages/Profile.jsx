c55b1baefaabdfb21a587bf209e73ad98d748817
Praneeth3717 <144830750+Praneeth3717@users.noreply.github.com>
2025-08-09 15:28:20 +0530

    Update Profile.jsx

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,13 @@
-import {jwtDecode} from "jwt-decode";
+import jwtDecode from "jwt-decode";
 
 export default function Profile() {
+  console.log("arrived to profile page")
   const token = localStorage.getItem("token");
-
+  console.log("fetching token")
   if (!token) return <p>Not logged in</p>;
-
+  console.log("got the token")
   const decoded = jwtDecode(token); // { userId, name, iat, exp }
-
+console.log("decoding the token")
   return (
     <div style={{ padding: 20 }}>
       <h1>Welcome, {decoded.name} 👋</h1>
